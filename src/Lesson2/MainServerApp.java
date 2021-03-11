913f99405ab63e2eff412119fe000994dfd0d139
IvanAntasyuk <Pikin64@gmail.com>
2021-03-11 10:56:33 +0300

    home work 2 part 1

---
+++ src/Lesson2/MainServerApp.java
@@ -0,0 +1,9 @@
+package Lesson2;
+
+import Lesson2.serverSide.service.MyServer;
+
+public class MainServerApp {
+    public static void main(String[] args) {
+        new MyServer();
+    }
+}
---
+++ src/Lesson2/serverSide/interfaces/AuthService.java
@@ -0,0 +1,9 @@
+package Lesson2.serverSide.interfaces;
+
+import java.sql.SQLException;
+
+public interface AuthService {
+    void start() throws ClassNotFoundException, SQLException;
+ void stop();
+    String getNickbyLoginandPassword(String login, String password);
+}
